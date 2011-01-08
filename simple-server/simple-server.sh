e3e46be45bf2e853fee45a8c4d8c80f833d0d4ff
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-08 16:29:48 -0500

    sending up helper files for chef

--- simple-server/simple-server.sh
+++ simple-server/simple-server.sh
@@ -10,4 +10,5 @@ gem install chef --version 0.9.12 --no-ri --no-rdoc
 
 git clone git://github.com/gorsuch/sandbox_cookbooks.git /tmp/sandbox_cookbooks
 
-cp packet/etc/profile.d/ruby.sh > /etc/profile.d/ruby.sh
+cp packet/etc/profile.d/ruby.sh /etc/profile.d/ruby.sh
+cp packet/ubuntu/* /home/ubuntu
