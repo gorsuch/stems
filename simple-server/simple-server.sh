55c48d2795754fdab192b2f4238d67a0531a33a8
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-08 16:20:10 -0500

    trying to use data in a packet

--- simple-server/simple-server.sh
+++ simple-server/simple-server.sh
@@ -10,4 +10,4 @@ gem install chef --version 0.9.12 --no-ri --no-rdoc
 
 git clone git://github.com/gorsuch/sandbox_cookbooks.git /tmp/sandbox_cookbooks
 
-export PATH=$PATH:/var/lib/gems/1.8/bin
+cat packet/etc/profile.d/ruby.sh > /etc/profile.d/ruby.sh
