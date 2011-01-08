858ef97f430ea388174ed717a664b85ab2e4dcde
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-08 16:26:58 -0500

    copy instead of cat

--- simple-server/simple-server.sh
+++ simple-server/simple-server.sh
@@ -10,4 +10,4 @@ gem install chef --version 0.9.12 --no-ri --no-rdoc
 
 git clone git://github.com/gorsuch/sandbox_cookbooks.git /tmp/sandbox_cookbooks
 
-cat packet/etc/profile.d/ruby.sh > /etc/profile.d/ruby.sh
+cp packet/etc/profile.d/ruby.sh > /etc/profile.d/ruby.sh
