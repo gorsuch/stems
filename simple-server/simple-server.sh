10405896e1416231d5f9159e1c1b43c7b208274a
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-08 15:59:10 -0500

    fetching chef recipes

--- simple-server/simple-server.sh
+++ simple-server/simple-server.sh
@@ -8,4 +8,6 @@ aptitude install --assume-yes "${packages[@]}"
 
 gem install chef --version 0.9.12 --no-ri --no-rdoc
 
+git clone git://github.com/gorsuch/sandbox_cookbooks.git /tmp/sandbox_cookbooks
+
 export PATH=$PATH:/var/lib/gems/1.8/bin
