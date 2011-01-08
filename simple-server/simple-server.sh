1fad4d6dffdc3e38e35e82a23c1346ad6b9fd11a
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-07 23:09:13 -0500

    going to assume that I don't really need libjson-ruby for this project

--- simple-server/simple-server.sh
+++ simple-server/simple-server.sh
@@ -3,7 +3,7 @@
 aptitude update
 declare -ar packages=( git-core
                        irb ruby rubygems1.8 ruby1.8-dev
-                       libopenssl-ruby libjson-ruby )
+                       libopenssl-ruby )
 aptitude install --assume-yes "${packages[@]}"
 
 gem install chef --version 0.9.12
