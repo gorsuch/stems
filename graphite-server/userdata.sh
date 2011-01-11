f4d1efb706e2e9fde002bc2d44501e1d89c1aa8a
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-10 20:11:38 -0500

    bad sourcing.

--- graphite-server/userdata.sh
+++ graphite-server/userdata.sh
@@ -11,7 +11,7 @@ function userdata() {
 	gem install chef --version 0.9.12 --no-ri --no-rdoc
 
 	cp packet/etc/profile.d/ruby.sh /etc/profile.d/ruby.sh
-	. ./etc/profile.d/ruby.sh
+	. /etc/profile.d/ruby.sh
 	cp packet/ubuntu/* /home/ubuntu
 	
 	git clone git://github.com/gorsuch/sandbox_cookbooks.git /home/ubuntu/sandbox_cookbooks
