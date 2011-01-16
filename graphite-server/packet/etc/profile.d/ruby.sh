deb640530c3ece8886c0b446c3f2dfc8c8dcd114
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-15 21:12:06 -0500

    created files inline so that I  can template easier and not worry about a 'packet' folder

--- graphite-server/packet/etc/profile.d/ruby.sh
+++
@@ -1,3 +0,0 @@
-export PATH="$PATH:/var/lib/gems/1.8/bin"
-export RUBYOPT="-Ku -rubygems"
-
--- graphite-server/userdata.sh
+++ graphite-server/userdata.sh
@@ -1,5 +1,35 @@
 #!/bin/bash -x
 
+export CHEF_ROOT=/root/chef
+export COOKBOOK_REPO=git://github.com/gorsuch/sandbox_cookbooks.git
+
+# super ugly-formatted function to create our files.
+# since the files are small... we can include them inline and also template them via 
+# simple vars.
+function create_files() {
+mkdir -p $CHEF_ROOT
+
+cat <<EOF > $CHEF_ROOT/node.json
+{
+  "run_list": [ "recipe[graphite]", "recipe[collectd]" ]
+}	
+EOF
+
+cat <<EOF > $CHEF_ROOT/solo.rb
+cookbook_path "$CHEF_ROOT/cookbooks"
+EOF
+
+cat <<EOF > /etc/rc.local
+. /etc/profile.d/ruby.sh
+cd $CHEF_ROOT/cookbooks && git pull && chef-solo -c $CHEF_ROOT/solo.rb -j $CHEF_ROOT/node.json
+EOF
+
+cat <<EOF > /etc/profile.d/ruby.sh
+export PATH="$PATH:/var/lib/gems/1.8/bin"
+export RUBYOPT="-Ku -rubygems"
+EOF
+}
+
 function userdata() {
 	echo BEGIN USERDATA
 	aptitude update
@@ -10,16 +40,14 @@ function userdata() {
 
 	gem install chef --version 0.9.12 --no-ri --no-rdoc
 
-	cp packet/etc/profile.d/ruby.sh /etc/profile.d/ruby.sh
+	create_files
+
 	. /etc/profile.d/ruby.sh
+
+	git clone $COOKBOOK_REPO $CHEF_ROOT/cookbooks
+	chef-solo -c $CHEF_ROOT/solo.rb -j $CHEF_ROOT/node.json
 	
-	mkdir -p /root/chef
-	cp packet/chef/* /root/chef
-	git clone git://github.com/gorsuch/sandbox_cookbooks.git /root/chef/cookbooks
-	chef-solo -c /root/chef/solo.rb -j /root/chef/node.json
-	
-	cp packet/etc/rc.local /etc/rc.local
 	echo END USERDATA
 }
-
+	
 userdata >> /var/log/userdata.log 2>&1
